b4756d55ed24976fad5fb25a2743dc373cde26f2
Borhan <brborhan70@gmail.com>
2023-06-13 00:56:49 +0600

    class delete api create success full

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.msnuvxp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -93,6 +93,16 @@ async function run() {
        const item =req.body;
        const result = await cartCollection.insertOne(item)
        res.send(result);
+    })
+
+
+
+    app.delete('/carts/:id', async(req,res)=>{
+      const id =req.params.id;
+      console.log(id)
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
 
